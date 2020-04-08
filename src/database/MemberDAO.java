21ee2db19ffc831d62a7ab6122f3d6dcbb823c47
soddi <cvb7412@naver.com>
2020-04-08 14:22:51 +0900

    memberVO

--- src/database/MemberDAO.java
+++ src/database/MemberDAO.java
@@ -2,7 +2,10 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 
 import oracle.net.nt.ConnectDescription;
 
@@ -36,7 +39,28 @@ public class MemberDAO {
 		
 	}
 	//데이터베이스 요청 작업
+	public Vector<MemberVO> getList() {
+		Vector<MemberVO> vecList = new Vector<MemberVO>();
 	
+		String sql = "select * from memberTBL";
+		try(Connection con =  getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery()) {
+			
+			while(rs.next()) {
+				MemberVO vo = new MemberVO();
+				vo.setNo(rs.getInt(1));
+				vo.setName(rs.getString(2));
+				vo.setAge(rs.getInt(3));
+				vo.setGender(rs.getString(4));
+				vecList.add(vo);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return vecList;
+	}
 }
 
 
